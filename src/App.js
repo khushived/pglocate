e56b1afdf996affb52caac2a2533820f12bd4bc4
khushived <115446582+khushived@users.noreply.github.com>
2023-06-16 05:05:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
 import './App.css';
 import Hero from './pages/home/Hero'
-
+import Login from './pages/login/Login';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
 function App() {
   return (
-    <div className="App">
-     <Hero />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
